342c940445dfc494be53c7ac3dd39a1f6e5a7b9f
surya gurjar <suryagurjar781@gmail.com>
2024-07-01 19:38:38 +0530

    terminal show data

--- project/surya_app/urls.py
+++ project/surya_app/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 # from surya_app import views
 from .views import *
 
-
 urlpatterns = [
     path('',register),
     path('registration/',registrationdata ,name='Registration'),
