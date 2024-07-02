8fabfe7ecc14382f58ab85201127079b468ba818
surya gurjar <suryagurjar781@gmail.com>
2024-07-02 18:36:46 +0530

    models include

--- project/surya_app/views.py
+++ project/surya_app/views.py
@@ -11,4 +11,9 @@ def registrationdata(request):
     # return HttpResponse(request,'registrationdata')  #server to views data 
     print (request.method)
     print (request.POST)
+    name=request.POST.get('name')
+    email=request.POST.get('email')
+    contact=request.POST.get('contact')
+    print(name,email,contact)
+    
     
