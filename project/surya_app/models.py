43bbeb3e0786b1e0a372f36de8a7fca527e1493c
surya gurjar <suryagurjar781@gmail.com>
2024-07-03 18:18:48 +0530

    table data show

--- project/surya_app/models.py
+++ project/surya_app/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+
+
 # Create your models here.
 class Registration(models.Model):   #model, forms.py ,serializers.py
     Name= models.CharField(max_length=50)
     Email= models.EmailField()
     Contact=models.IntegerField()
+   
     
--- project/surya_app/views.py
+++ project/surya_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Registration
+
 
 
 # Create your views here.
@@ -8,12 +10,14 @@ def register(request):
 
 
 def registrationdata(request):
-    # return HttpResponse(request,'registrationdata')  #server to views data 
+    
     print (request.method)
     print (request.POST)
     name=request.POST.get('name')
     email=request.POST.get('email')
     contact=request.POST.get('contact')
     print(name,email,contact)
+    Registration.objects.create(Name=name,Email=email,Contact=contact)
+    print("Data Save Successfully")
     
     
